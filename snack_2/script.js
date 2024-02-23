d1845ff992a785da503fdaa9719f1ed1d4daefba
SimoneCerri <iosonosimone@hotmail.it>
2024-02-23 12:36:26 +0100

    done

---
+++ snack_2/script.js
@@ -0,0 +1,11 @@
+let firstString = prompt("Can you give me one word please ?");
+let secondString = prompt("Can you give a second word ?");
+
+if (firstString.length < secondString.length)
+{
+    console.log(firstString);
+}
+else
+{
+    console.log(secondString);
+}
