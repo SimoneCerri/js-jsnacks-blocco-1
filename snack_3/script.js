71f20a48c7fdb3ff247b9711f3c8d84dda9ccd65
SimoneCerri <iosonosimone@hotmail.it>
2024-02-26 11:32:24 +0100

    ,

---
+++ snack_3/script.js
@@ -0,0 +1,21 @@
+let number;
+let numbersArray = [];
+let sumNumbers = 0;
+
+/* for (let index = 0; index < 10; index++)
+{
+    number = prompt("Let me know a number");
+    numbersArray.add(number);
+    sumNumbers += number;
+    console.log(number,numbersArray,sumNumbers);
+} */
+
+for (let index = 0; index < 10 ; index++)
+{
+    number = Number(prompt("Let me know a number"));
+    const element = numbersArray[index];
+    
+}
+
+//sbagliato pd 
+
