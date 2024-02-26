d0af2ca33970a9def12e01b4c637f2dffa091ac5
SimoneCerri <iosonosimone@hotmail.it>
2024-02-26 12:31:22 +0100

    done

---
+++ snack_6/script.js
@@ -0,0 +1,10 @@
+let enne = Number(prompt("How many number do you want to calculate ?"));
+let cubes;
+
+while (enne > 0)
+{
+    let number = prompt("give me number")
+    cubes = ( number * number * number );
+    console.log(cubes);
+    enne--;
+}
